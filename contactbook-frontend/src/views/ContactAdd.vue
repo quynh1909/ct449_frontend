441f8f122fb0e40c41c1ff5367f794eca6e57982
quynh1909 <quynhb2014603@student.ctu.edu.vn>
2024-03-28 13:46:36 +0700

    Them lien he moi

---
+++ contactbook-frontend/src/views/ContactAdd.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="page">
+        <h4>Thêm mới Liên hệ</h4>
+        <ContactAddForm :contact="null" @submit:contact="addContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+import ContactAddForm from "@/components/ContactAddForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+    components: {
+        ContactAddForm,
+    },
+    data() {
+        return {
+            message: "",
+        };
+    },
+    methods: {
+        async addContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Liên hệ được thêm mới thành công.";
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
